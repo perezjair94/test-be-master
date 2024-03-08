a820cd8bbf2a0e8862dcf848894addbe8f32c797
Jair Pérez <oquendojair10@gmail.com>
2024-03-08 10:13:16 -0500

    feat: add placeholder data with users list

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,10 @@
+const users = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    name: "User",
+    email: "user@mail.com",
+    password: "123456",
+  },
+];
+
+module.exports = { users };
