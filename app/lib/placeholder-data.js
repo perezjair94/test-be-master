01cb4744b1100b46ae08e0d76f2850c8f131fe9a
Jair Pérez <oquendojair10@gmail.com>
2024-03-08 20:40:00 -0500

    feat(placeholder-data): add categories list

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -7,4 +7,35 @@ const users = [
   },
 ];
 
-module.exports = { users };
+const categories = [
+  {
+    id: 'c1b97fc5-0f0b-4f17-aae8-7e5418a92a3f',
+    name: 'X-MEN',
+    image:
+      'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/06/mejores-comics-x-men.jpg?tf=1920x',
+  },
+  {
+    id: '9d4a3d2e-aa89-4e1f-bad1-17c8fe36c5d1',
+    name: '4 FANTASTICOS',
+    image: 'https://i.blogs.es/03895e/4-fantasticos/1366_2000.webp',
+  },
+  {
+    id: 'e3984b4b-63d7-4f29-b415-8d05b6f4d56e',
+    name: 'AVENGERS',
+    image:
+      'https://www.latercera.com/resizer/9dqTBzKNNKKu48Tw1AHiBXnEZCg=/900x600/filters:focal(415x390:425x380)/cloudfront-us-east-1.images.arcpublishing.com/copesa/YELICM4GFZCPJGQ7GATPQRWDXA.jpg',
+  },
+  {
+    id: 'a6825a9f-91d3-42c8-b82e-8fe95a3a0bf1',
+    name: 'GUARDIANES DE LA GALEXIA',
+    image: 'https://i.blogs.es/c84032/screenshot_1858/1366_2000.webp',
+  },
+  {
+    id: '3f8f1fb3-6f1d-4e8a-bc27-0a6d27a4374d',
+    name: 'IRO MAN',
+    image:
+      'https://tainthemeat.files.wordpress.com/2016/09/iron-man-126-banner.jpg?w=1024&h=567&crop=1',
+  },
+];
+
+module.exports = { users, categories };
