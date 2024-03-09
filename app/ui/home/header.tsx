42e6bc4592624d5b1adc3275869b378aebebcdea
Jair Pérez <oquendojair10@gmail.com>
2024-03-08 20:38:21 -0500

    feat: create header component

---
+++ app/ui/home/header.tsx
@@ -0,0 +1,25 @@
+import { signOut } from '@/auth';
+import { LogOut } from 'lucide-react';
+
+export default function Header() {
+  return (
+    <header className="mx-auto max-w-screen-xl p-5 px-10">
+      <div className="flex justify-between">
+        <h2 className="font-semibold">Movie App</h2>
+        <form
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
+        >
+          <button className="text-sm">
+            <div className="flex items-center gap-2">
+              <LogOut className="h-4 w-4" />
+              Sign Out
+            </div>
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
