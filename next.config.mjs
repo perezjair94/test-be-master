a24d7856e2ba005a3079c21c3b569d8f6c93fcc6
Jair Pérez <oquendojair10@gmail.com>
2024-03-09 16:47:51 -0500

    refactor(next.config): remove old hostname

--- next.config.mjs
+++ next.config.mjs
@@ -4,36 +4,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cdn.hobbyconsolas.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.blogs.es',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.latercera.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'tainthemeat.files.wordpress.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'disney.images.edge.bamgrid.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'm.media-amazon.com',
         port: '',
         pathname: '/**',
