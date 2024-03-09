b9479413ad44a63ab44af712fa9e5e07da5daf74
Jair Pérez <oquendojair10@gmail.com>
2024-03-09 10:11:20 -0500

    feat(data): add fetch single category and movies

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,6 @@
 import { unstable_noStore as noStore } from 'next/cache';
 import { fetcher } from './utils';
-import { Category } from './definitions';
+import { Category, Movie } from './definitions';
 
 export async function fetchCategories(): Promise<Category[] | undefined> {
   noStore();
@@ -10,7 +10,52 @@ export async function fetchCategories(): Promise<Category[] | undefined> {
     });
     return response;
   } catch (error) {
-    console.error('Database Error:', error);
+    console.error('Fetch Error:', error);
     throw new Error('Failed to fetch category data.');
+  }
+}
+
+export async function fetchCategory(category: Category['id']) {
+  noStore();
+  try {
+    const response = await fetcher(`/categories/${category}`, {
+      next: { tags: ['category'] },
+    });
+    return response;
+  } catch (error) {
+    console.error('Fetch Error:', error);
+    throw new Error('Failed to fetch category data.');
+  }
+}
+
+export async function fetchMovies(
+  category: Category['id'],
+): Promise<Movie[] | undefined> {
+  noStore();
+  try {
+    const response = await fetcher(`/movies?category=${category}`, {
+      next: { tags: ['movies'] },
+    });
+    return response;
+  } catch (error) {
+    console.error('Fetch Error:', error);
+    throw new Error('Failed to fetch movies data.');
+  }
+}
+
+export async function fetchHomeData(category: Category['id']) {
+  noStore();
+  try {
+    const data = await Promise.all([
+      fetchCategory(category),
+      fetchMovies(category),
+    ]);
+    return {
+      category: data[0],
+      movies: data[1],
+    };
+  } catch (error) {
+    console.error('Fetch Error:', error);
+    throw new Error('Failed to fetch homde data.');
   }
 }
