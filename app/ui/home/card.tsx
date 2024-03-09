e9f5bfd1e8df7d825c10b8167ac968ca2ae68e8c
Jair Pérez <oquendojair10@gmail.com>
2024-03-09 10:10:36 -0500

    feat: create categories component

--- app/ui/home/card.tsx
+++ app/ui/home/card.tsx
@@ -12,15 +12,15 @@ export default function Card({ id, name, image }: Category) {
   const active = Boolean(category === id);
   return (
     <Link href={`/home?category=${id}`} scroll={false}>
-      <div className="group relative cursor-pointer overflow-hidden rounded-xl shadow-lg transition-all hover:ring hover:ring-yellow-500 hover:ring-offset-2">
+      <div className="group relative cursor-pointer overflow-hidden rounded-xl shadow-lg transition-all hover:ring hover:ring-white/90 hover:ring-offset-2 hover:ring-offset-[#141414]">
         <Image
           src={image}
           alt="category-image"
           className={clsx(
-            'absolute object-cover group-hover:scale-125',
+            'absolute object-cover transition-all group-hover:scale-125',
             active
               ? 'scale-125 brightness-75 grayscale-0'
-              : 'brightness-[30%] grayscale group-hover:brightness-75 group-hover:grayscale-0',
+              : 'brightness-50 grayscale group-hover:brightness-75 group-hover:grayscale-0',
           )}
           fill
         />
--- app/ui/home/categories.tsx
+++ app/ui/home/categories.tsx
@@ -1,10 +1,16 @@
-import Cards from './cards';
+import Card from './card';
+import { fetchCategories } from '@/app/lib/data';
 
-export default function Categories() {
+export default async function Categories() {
+  const categories = await fetchCategories();
+  if (!categories || categories.length === 0)
+    return <span>No available data</span>;
   return (
     <section className="mx-auto max-w-screen-xl">
-      <div className="mt-[30px] grid grid-cols-5 gap-[20px]">
-        <Cards />
+      <div className="mt-[30px] grid grid-cols-5 gap-4">
+        {categories.map((category) => (
+          <Card key={category.id} {...category} />
+        ))}
       </div>
     </section>
   );
