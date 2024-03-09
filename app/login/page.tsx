7352c13b8432ed7febb4eac2604ee7512be1248e
Jair Pérez <oquendojair10@gmail.com>
2024-03-08 20:40:33 -0500

    feat: create login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,10 @@
-import LoginForm from "@/app/ui/login-form";
+'use client';
+
+import LoginForm from '@/app/ui/login-form';
 
 export default function Page() {
   return (
-    <main className="flex h-screen items-center justify-center bg-[url(/background-2.jpg)] bg-cover bg-no-repeat">
+    <main className="flex h-screen items-center justify-center bg-[url(/background.jpg)] bg-cover bg-no-repeat">
       <div className="relative mx-auto flex w-full max-w-[500px] flex-col space-y-2.5 rounded-[20px] bg-white p-10">
         <h2 className="w-32 text-2xl font-bold md:w-36">Movie App</h2>
         <p className="text-sm">
