dd3b6fb1ebb7a981a2e853dc17ce12764a374ac6
Jair Pérez <oquendojair10@gmail.com>
2024-03-10 10:02:45 -0500

    refactor(auth): remove sql imports

--- auth.ts
+++ auth.ts
@@ -1,21 +1,8 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import bcrypt from 'bcrypt';
-import { sql } from '@vercel/postgres';
 import { z } from 'zod';
-import type { User } from '@/app/lib/definitions';
 import { users } from '@/app/lib/placeholder-data';
 import { authConfig } from './auth.config';
-
-// async function getUser(email: string): Promise<User | undefined> {
-//   try {
-//     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-//     return user.rows[0];
-//   } catch (error) {
-//     console.error("Failed to fetch user:", error);
-//     throw new Error("Failed to fetch user.");
-//   }
-// }
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
